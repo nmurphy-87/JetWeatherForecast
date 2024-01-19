82ff643b4c9d0ced1a0e272665a3fdab1616a3df
Niall <niall@Aarons-MacBook-Air.local>
2024-01-19 15:20:45 +0000

    set up nav for splash screen

--- app/src/main/java/com/niallmurph/jetweatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/niallmurph/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -1,9 +1,10 @@
 package com.niallmurph.jetweatherforecast.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun WeatherSplashScreen(navController: NavController){
-
+    Text("Splash Screen")
 }
