e5d74e972606206a799c3dfe6c0f141aaac6afb2
Niall <niall@Aarons-MacBook-Air.local>
2024-01-22 17:00:53 +0000

    Added TopAppBar widget

--- app/src/main/java/com/niallmurph/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/niallmurph/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,34 +1,57 @@
-package com.niallmurph.jetweatherforecast.screens
+package com.niallmurph.jetweatherforecast.screens.main
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.niallmurph.jetweatherforecast.data.DataOrException
 import com.niallmurph.jetweatherforecast.model.Weather
-import com.niallmurph.jetweatherforecast.screens.main.MainViewModel
+import com.niallmurph.jetweatherforecast.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController, viewModel: MainViewModel = hiltViewModel()) {
 
-    ShowData(mainViewModel = viewModel)
-}
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
-
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
-        value = mainViewModel.getWeatherData(city = "dublin")
+        value = viewModel.getWeatherData(city = "dublin")
     }.value
 
-    if(weatherData.loading == true) {
+    if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text("Main Screen: ${weatherData.data.toString()}")
+        MainScaffold(weather = weatherData.data!!, navController = navController)
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            WeatherAppBar(
+                title = "${weather.city.name}, ${weather.city.country}",
+                navController = navController,
+                elevation = 4.dp
+            ) {
+            }
+        }
+    ) {
+        MainContent(data = weather)
     }
 
+}
+
+@Composable
+fun MainContent(data: Weather) {
+    Text(text = data.city.name)
 }
---
+++ app/src/main/java/com/niallmurph/jetweatherforecast/widgets/WeatherAppBar.kt
@@ -0,0 +1,66 @@
+package com.niallmurph.jetweatherforecast.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.IconCompat
+import androidx.navigation.NavController
+
+@Preview
+@Composable
+fun WeatherAppBar(
+    title: String = "Title",
+    icon: ImageVector? = null,
+    onMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = MaterialTheme.colors.onSecondary,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            )
+        },
+        actions = {
+            if(onMainScreen){
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More Icon")
+                }
+            } else Box{}
+        },
+        navigationIcon = {
+            if(icon != null) {
+                Icon(imageVector = icon, contentDescription = null,
+                    modifier = Modifier.clickable {
+                        onButtonClicked.invoke()
+                    },
+                tint = MaterialTheme.colors.onSecondary,)
+            }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = elevation
+    )
+}
