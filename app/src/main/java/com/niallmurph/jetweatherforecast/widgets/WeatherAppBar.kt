e383dbe2b2fdc383c420e712748e0d987b54293f
Niall <niall@Aarons-MacBook-Air.local>
2024-01-24 15:13:25 +0000

    Added dropdown dialog and 3 selectable options

--- app/src/main/java/com/niallmurph/jetweatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/niallmurph/jetweatherforecast/widgets/WeatherAppBar.kt
@@ -1,12 +1,13 @@
 package com.niallmurph.jetweatherforecast.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -16,7 +17,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.drawable.IconCompat
 import androidx.navigation.NavController
 
 @Preview
@@ -30,6 +30,13 @@ fun WeatherAppBar(
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {}
 ) {
+
+    val showDialog = remember { mutableStateOf(false) }
+
+    if (showDialog.value) {
+        ShowSettingsDropDown(showDialog = showDialog, navController = navController)
+    }
+
     TopAppBar(
         title = {
             Text(
@@ -42,25 +49,81 @@ fun WeatherAppBar(
             )
         },
         actions = {
-            if(onMainScreen){
-                IconButton(onClick = { onAddActionClicked.invoke()}) {
+            if (onMainScreen) {
+                IconButton(onClick = { onAddActionClicked.invoke() }) {
                     Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
                 }
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { showDialog.value = true }) {
                     Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More Icon")
                 }
-            } else Box{}
+            } else Box {}
         },
         navigationIcon = {
-            if(icon != null) {
-                Icon(imageVector = icon, contentDescription = null,
+            if (icon != null) {
+                Icon(
+                    imageVector = icon, contentDescription = null,
                     modifier = Modifier.clickable {
                         onButtonClicked.invoke()
                     },
-                tint = MaterialTheme.colors.onSecondary,)
+                    tint = MaterialTheme.colors.onSecondary,
+                )
             }
         },
         backgroundColor = Color.Transparent,
         elevation = elevation
     )
+}
+
+@Composable
+fun ShowSettingsDropDown(
+    showDialog: MutableState<Boolean>,
+    navController: NavController
+) {
+    var expanded by remember { mutableStateOf(true) }
+    val items = listOf(
+        "About",
+        "Favourites",
+        "Settings"
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 40.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(160.dp)
+                .background(Color.White)
+        ) {
+            items.forEachIndexed { index, item ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(
+                        imageVector = when (item) {
+                            "About" -> Icons.Default.Info
+                            "Favourites" -> Icons.Default.Favorite
+                            else -> Icons.Default.Settings
+                        },
+                        contentDescription = null,
+                        tint = Color.LightGray
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = item,
+                        modifier = Modifier
+                            .clickable {
+
+                            },
+                        fontWeight = FontWeight.W300
+                    )
+                }
+            }
+        }
+    }
 }
