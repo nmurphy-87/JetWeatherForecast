a3581f7a774540dfeb94549ccfcda05dae5aa2cf
Niall <niall@Aarons-MacBook-Air.local>
2024-01-25 16:20:38 +0000

    Added Settings table to DB, and calls to DAO and repository, ViewModel also set up

--- app/src/main/java/com/niallmurph/jetweatherforecast/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/niallmurph/jetweatherforecast/screens/settings/SettingsScreen.kt
@@ -1,13 +1,19 @@
 package com.niallmurph.jetweatherforecast.screens.settings
 
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
 fun SettingsScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: SettingsViewModel = hiltViewModel()
 ){
     Text("Settings")
-
+    
+//    Scaffold() {
+//
+//    }
 }
